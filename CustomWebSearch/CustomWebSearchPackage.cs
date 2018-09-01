0be902f3338d9f2765415f4d58aac589b1f0a7e7
appastoy82 <appastoy82@gmail.com>
2018-09-01 12:55:23 +0900

    Support save load.

--- CustomWebSearch/CustomWebSearchPackage.cs
+++ CustomWebSearch/CustomWebSearchPackage.cs
@@ -73,8 +73,8 @@ namespace CustomWebSearch
 		{
 			// When initialized asynchronously, the current thread may be a background thread at this point.
 			// Do any initialization that requires the UI thread after switching to the UI thread.
-			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-			BindMenus(); 
+			await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            BindMenus(); 
 		}
 
 		void BindMenus()
--- CustomWebSearch/OptionPage.cs
+++ CustomWebSearch/OptionPage.cs
@@ -1,50 +1,68 @@
 ﻿using Microsoft.VisualStudio.Shell;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace CustomWebSearch
 {
-	[System.Serializable]
-	public class QueryData
-	{
-		public QueryTemplateType TemplateType { get; set; }
-        public string CustomTemplateName { get; set; }
-        public string QueryFormat { get; set; }
-
-		public QueryData()
-		{
-
-		}
-
-		public QueryData(QueryTemplateType type)
-		{
-			TemplateType = type;
-			QueryFormat = Constants.QueryTemplateFormats[(int)TemplateType];
-		}
-	}
-
 	public class OptionPage : DialogPage
 	{
 		readonly OptionPageControl optionPageControl;
 
+        public const int QueryCount = 10;
+
 		public WebBrowserType WebBrowserType { get; set; }
 		public string CustomWebBrowserPath { get; set; } = string.Empty;
-		public bool IsInitialized { get; set; } = false;
+        public bool Initialized { get; set; }
 
-		public QueryData[] Queries { get; set; } = new QueryData[10]
-		{
-			new QueryData(QueryTemplateType.GoogleSearch),
-			new QueryData(QueryTemplateType.GoogleTranslate),
-            new QueryData(QueryTemplateType.MicrosoftMSDN),
-            new QueryData(QueryTemplateType.StackOverflow),
-			new QueryData(QueryTemplateType.Unity3dDocument),
-			new QueryData(QueryTemplateType.Unity3dAnswer),
-			new QueryData(),
-			new QueryData(),
-			new QueryData(),
-			new QueryData()
-		};
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public QueryData[] Queries { get; set; } = new QueryData[QueryCount]
+        {
+            new QueryData(),
+            new QueryData(),
+            new QueryData(),
+            new QueryData(),
+            new QueryData(),
+            new QueryData(),
+            new QueryData(),
+            new QueryData(),
+            new QueryData(),
+            new QueryData()
+        };
 
-		public OptionPage()
+        public QueryTemplateType TemplateType1 { get; set; }
+        public QueryTemplateType TemplateType2 { get; set; }
+        public QueryTemplateType TemplateType3 { get; set; }
+        public QueryTemplateType TemplateType4 { get; set; }
+        public QueryTemplateType TemplateType5 { get; set; }
+        public QueryTemplateType TemplateType6 { get; set; }
+        public QueryTemplateType TemplateType7 { get; set; }
+        public QueryTemplateType TemplateType8 { get; set; }
+        public QueryTemplateType TemplateType9 { get; set; }
+        public QueryTemplateType TemplateType10 { get; set; }
+
+        public string CustomTemplateName1 { get; set; } = string.Empty;
+        public string CustomTemplateName2 { get; set; } = string.Empty;
+        public string CustomTemplateName3 { get; set; } = string.Empty;
+        public string CustomTemplateName4 { get; set; } = string.Empty;
+        public string CustomTemplateName5 { get; set; } = string.Empty;
+        public string CustomTemplateName6 { get; set; } = string.Empty;
+        public string CustomTemplateName7 { get; set; } = string.Empty;
+        public string CustomTemplateName8 { get; set; } = string.Empty;
+        public string CustomTemplateName9 { get; set; } = string.Empty;
+        public string CustomTemplateName10 { get; set; } = string.Empty;
+
+        public string QueryFormat1 { get; set; } = string.Empty;
+        public string QueryFormat2 { get; set; } = string.Empty;
+        public string QueryFormat3 { get; set; } = string.Empty;
+        public string QueryFormat4 { get; set; } = string.Empty;
+        public string QueryFormat5 { get; set; } = string.Empty;
+        public string QueryFormat6 { get; set; } = string.Empty;
+        public string QueryFormat7 { get; set; } = string.Empty;
+        public string QueryFormat8 { get; set; } = string.Empty;
+        public string QueryFormat9 { get; set; } = string.Empty;
+        public string QueryFormat10 { get; set; } = string.Empty;
+
+        public OptionPage()
 		{
 			optionPageControl = new OptionPageControl(this);
 		}
@@ -83,7 +101,65 @@ namespace CustomWebSearch
 		public override void LoadSettingsFromStorage()
 		{
 			base.LoadSettingsFromStorage();
+            InitializeIfNot();
+            OnAfterLoad();
+
 			optionPageControl.UpdateProperties();
 		}
-	}
+
+        public override void SaveSettingsToStorage()
+        {
+            OnBeforeSave();
+            base.SaveSettingsToStorage();
+        }
+
+        void InitializeIfNot()
+        {
+            if (!Initialized)
+            {
+                Queries = new QueryData[10]
+                {
+                    new QueryData(QueryTemplateType.GoogleSearch),
+                    new QueryData(QueryTemplateType.GoogleTranslate),
+                    new QueryData(QueryTemplateType.MicrosoftMSDN),
+                    new QueryData(QueryTemplateType.StackOverflow),
+                    new QueryData(QueryTemplateType.Unity3dDocument),
+                    new QueryData(QueryTemplateType.Unity3dAnswer),
+                    new QueryData(),
+                    new QueryData(),
+                    new QueryData(),
+                    new QueryData()
+                };
+                Initialized = true;
+                OnBeforeSave();
+                SaveSettingsToStorage();
+            }
+        }
+
+        void OnBeforeSave()
+        {
+            var type = GetType();
+            for (int i = 0; i < QueryCount; i++)
+            {
+                var query = Queries[i];
+                var indexString = (i + 1).ToString();
+                type.GetProperty("TemplateType" + indexString).GetSetMethod().Invoke(this, new object[] { query.TemplateType });
+                type.GetProperty("CustomTemplateName" + (i + 1).ToString()).GetSetMethod().Invoke(this, new object[] { query.CustomTemplateName });
+                type.GetProperty("QueryFormat" + (i + 1).ToString()).GetSetMethod().Invoke(this, new object[] { query.QueryFormat });
+            }
+        }
+
+        void OnAfterLoad()
+        {
+            var type = GetType();
+            for (int i = 0; i < QueryCount; i++)
+            {
+                var query = Queries[i];
+                var indexString = (i + 1).ToString();
+                query.TemplateType = (QueryTemplateType)type.GetProperty("TemplateType" + indexString).GetGetMethod().Invoke(this, null);
+                query.CustomTemplateName = (string)type.GetProperty("CustomTemplateName" + (i + 1).ToString()).GetGetMethod().Invoke(this, null);
+                query.QueryFormat = (string)type.GetProperty("QueryFormat" + (i + 1).ToString()).GetGetMethod().Invoke(this, null);
+            }
+        }
+    }
 }
